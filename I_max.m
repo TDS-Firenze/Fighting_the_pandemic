f769bee7153e8d61917ca16f1dc44f3af079a0a6
TDS-Firenze <67388177+TDS-Firenze@users.noreply.github.com>
2020-06-29 09:19:28 +0200

    Update I_max.m

--- I_max.m
+++ I_max.m
@@ -1,13 +1,16 @@
+% This function plot Imax (the maximum number of infections) as a function of R_0
+
 S0 = 0.99;
 I0 = 0.01;
 
 R0 = linspace(1, 50, 1000);
 
 plot(R0, Imax(R0,S0,I0),'LineWidth',2)
+
 xlabel('R_0','Fontsize',15);
 ylabel('I_{max}','Fontsize',15);
 
-%%% Definition of I_max %%%
+% Definition of I_max 
 function I = Imax(R0,S0,I0)
  I = I0 + S0  -  S0./ R0 .* (1 + log( R0 ) ); 
 end
