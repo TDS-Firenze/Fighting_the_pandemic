185baeae1bf5a8f9890150bef79bf16bcbcb4581
TDS-Firenze <67388177+TDS-Firenze@users.noreply.github.com>
2020-06-29 09:32:57 +0200

    Add files via upload

---
+++ SIR_model_3.m
@@ -0,0 +1,52 @@
+clear all
+
+%for sigma = 0.01: 0.01 : 0.99 
+sigma = 0.51;
+fpr   = 0.2553191;
+fnr   = 0.03846154;
+alfaS = sigma * (1 - fpr);
+alfaI = sigma * (1 - fnr);
+gamma = 0.01;
+beta  = 4;
+
+tspan = linspace(0,10,1000) ;
+y0 = [0.999 0.001 0] ;
+[t,y] = ode45(@(t,y) SIR3(t,y,alfaS,alfaI,beta,gamma), tspan, y0);
+
+
+plot(t,y(:,1),'LineWidth',2,'Color','green')
+hold on
+plot(t,y(:,2),'LineWidth',2,'Color','blue')
+plot(t,y(:,3),'LineWidth',2,'Color','red')
+xlabel('Time t','Fontsize',15);
+
+
+sigma = 0.99;
+fpr   = 0.2553191;
+fnr   = 0.03846154;
+alfaS = sigma * (1 - fpr);
+alfaI = sigma * (1 - fnr);
+gamma = 0.01;
+beta  = 4;
+
+tspan = linspace(0,10,1000) ;
+y0 = [0.999 0.001 0] ;
+[t,y] = ode45(@(t,y) SIR3(t,y,alfaS,alfaI,beta,gamma), tspan, y0);
+
+plot(t,y(:,1),':','LineWidth',2,'Color','green')
+plot(t,y(:,2),':','LineWidth',2,'Color','blue')
+plot(t,y(:,3),':','LineWidth',2,'Color','red')
+xlabel('Time t','Fontsize',15);
+legend('S(t)','I(t)','R(t)','Fontsize',15)
+
+
+function  F = SIR3(t,y,alfaS,alfaI,beta,gamma)
+% S' = - beta I S - alfaS S
+% I' =   beta I S - gamma I - alfaI I 
+% R' =   alfaI I   +  gamma I + alfaS S
+
+F = [- beta * y(1) * y(2) - alfaS * y(1); ...
+     beta * y(1) * y(2) - gamma * y(2) - alfaI * y(2); ...
+     alfaI * y(2) + gamma * y(2) + alfaS * y(1)];
+end
+
