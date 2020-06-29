f749feef44dff218637bbf9d3bb8550f5e950238
TDS-Firenze <67388177+TDS-Firenze@users.noreply.github.com>
2020-06-29 09:36:34 +0200

    Update and rename SIR_model_3.m to SIR_model_tests.m

--- SIR_model_tests.m
+++ SIR_model_tests.m
@@ -1,9 +1,12 @@
+% This function solve the SIR model with tests
+
+
 clear all
 
 %for sigma = 0.01: 0.01 : 0.99 
 sigma = 0.51;
-fpr   = 0.2553191;
-fnr   = 0.03846154;
+fpr   = 0.2553191;                 % False Positive Rate
+fnr   = 0.03846154;                % False Negative Rate
 alfaS = sigma * (1 - fpr);
 alfaI = sigma * (1 - fnr);
 gamma = 0.01;
@@ -37,9 +40,10 @@ plot(t,y(:,1),':','LineWidth',2,'Color','green')
 plot(t,y(:,2),':','LineWidth',2,'Color','blue')
 plot(t,y(:,3),':','LineWidth',2,'Color','red')
 xlabel('Time t','Fontsize',15);
-legend('S(t)','I(t)','R(t)','Fontsize',15)
+legend('S','I','R','S with \sigma = 0.99 ','I with \sigma = 0.99', 'R with \sigma = 0.99','Fontsize',16)
 
 
+% Definition of function 
 function  F = SIR3(t,y,alfaS,alfaI,beta,gamma)
 % S' = - beta I S - alfaS S
 % I' =   beta I S - gamma I - alfaI I 
