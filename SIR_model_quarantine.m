d3bd8a46aa14ab40b9ea70995596b10f4bc1c67a
TDS-Firenze <67388177+TDS-Firenze@users.noreply.github.com>
2020-06-29 09:25:05 +0200

    Update and rename SIR_model_2.m to SIR_model_quarantine.m

--- SIR_model_quarantine.m
+++ SIR_model_quarantine.m
@@ -1,7 +1,9 @@
+% This function solve the SIR model with quarantine effect
+
 clear all
 
 tspan = linspace(0,10,1000) ;
-y0 = [0.999 0.001 0] ;
+y0 = [0.999 0.001 0] ;         % 0.1% of initial population is infected
 
 alfa = 0.01;
 gamma = 1;
@@ -15,7 +17,6 @@ hold on
 plot(t,y(:,2),'LineWidth',2,'Color','blue')
 plot(t,y(:,3),'LineWidth',2,'Color','red')
 
- 
 
 alfa = 0.99;
 gamma = 1;
@@ -27,15 +28,16 @@ beta = 3;
 plot(t,y(:,1),':','LineWidth',2,'Color','green')
 plot(t,y(:,2),':','LineWidth',2,'Color','blue')
 plot(t,y(:,3),':','LineWidth',2,'Color','red')
+
+
 xlabel('Time t','Fontsize',16);
 legend('S','I','R','S quarantine','I quarantine', 'R quarantine','Fontsize',16)
 
-
+% Definition of the function
 function  F = SIR2(t,y,alfa,beta,gamma)
 % S' = - beta I S
 % I' =   beta I S - gamma I - alfa I
 % R' =   alfa I   +  gamma I
-
 F = [- beta * y(1) * y(2); %
      beta * y(1) * y(2) - gamma * y(2) - alfa * y(2); %
      alfa * y(2) + gamma * y(2)];
